cb85994ccf1d128c170a7df79fc9bf00da0f02e0
Max-Qiu <qzhing@126.com>
2022-05-22 22:38:57 +0800

    1. 添加 商品类目-获取树结构 接口 2. 添加 MybatisPlus 、 Redis 、 Cache 、 全局异常处理 等配置 3. 添加 OpenFeign服务调用 、 网关转发商品服务的配置

--- max-mall-product/src/main/java/com/maxqiu/mall/product/ProductApplication.java
+++ max-mall-product/src/main/java/com/maxqiu/mall/product/ProductApplication.java
@@ -1,14 +1,18 @@
 package com.maxqiu.mall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * 商品 服务
  *
  * @author Max_Qiu
  */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.maxqiu.mall.product", "com.maxqiu.mall.common"})
+@MapperScan("com.maxqiu.mall.product.mapper")
+@EnableCaching
 public class ProductApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductApplication.class, args);
